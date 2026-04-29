72442ae5624b5802b5ac21088881a46322fe8b89
Thavanayagam Pinegash <thavanayagampinegash@Pinegashs-MacBook-Air.local>
2026-04-29 16:34:13 +0530

    updated

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5005/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://practice1-production-1160.up.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
